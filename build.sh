b899c58d2ff9ab28e3eb1c802d7dbe5a9cdb69a4
Mark Hill <mark@astranis.com>
2020-10-17 22:14:22 -0700

    WORKING!

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 set -o pipefail
-INCLUDES="-I external/STM32CubeL4/Drivers/CMSIS/Device/ST/STM32L4xx/Include/ -I external/STM32CubeL4/Drivers/CMSIS/Include/ -I ."
-./toolchain/arm/bin/arm-none-eabi-gcc -c main.cc -o main.o $INCLUDES -DSTM32L432xx=1 -mthumb -mcpu=cortex-m4 --specs=nano.specs --specs=nosys.specs -I external/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Inc/
+INCLUDES="-I external/STM32CubeL4/Drivers/CMSIS/Include/ -I ."
+./toolchain/arm/bin/arm-none-eabi-gcc -c main.cc -o main.o $INCLUDES -mthumb -mcpu=cortex-m4 --specs=nosys.specs -I external/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Inc/
 ./toolchain/arm/bin/arm-none-eabi-gcc -c -o startup_ARMCM4.o toolchain/arm/share/gcc-arm-none-eabi/samples/startup/startup_ARMCM4.S
-./toolchain/arm/bin/arm-none-eabi-gcc main.o startup_ARMCM4.o -o test.elf $INCLUDES -DSTM32L432xx=1 -mthumb -mcpu=cortex-m4 --specs=nano.specs --specs=nosys.specs -I external/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Inc/ -Tsections.ld
+./toolchain/arm/bin/arm-none-eabi-gcc main.o startup_ARMCM4.o -o test.elf $INCLUDES -mthumb -mcpu=cortex-m4 --specs=nosys.specs -I external/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Inc/ -Tsections.ld
--- main.cc
+++ main.cc
@@ -1,46 +1,23 @@
 #include <cstdint>
 
 #include "external/STM32CubeL4/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l432xx.h"
-// XXX
-// XXX#include
-// "external/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h"
 // XXX do I have hardfloat?
 // https://launchpadlibrarian.net/170926122/readme.txt
 
 int main(int argc, char** argv) {
-  // XXX replace
   RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
-  GPIOB->MODER = 0xFFFFFEAF;
-  int delay = 10'000;
+  GPIOB->MODER = 0xFFFFF27F;
+  GPIOB->PUPDR = 0x2 << 10;
+  bool value = true;
   while (1) {
-    GPIOB->ODR |= 0U << 3;
-    delay = 10'000;
-    while (--delay)
-      ;
-    GPIOB->ODR |= 1U << 3;
-    delay = 10'000;
-    while (--delay)
+    // XXX uint32_t value = ((GPIOB->IDR >> 5) & 0x1);
+    GPIOB->BSRR = (value << 3) | (((~value) & 0x1) << (16 + 3));
+    value = !value;
+    int delay = 10'000;
+    while (delay--)
       ;
   }
-  /*
-  RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN | RCC_AHB2ENR_GPIOBEN |
-                  RCC_AHB2ENR_GPIOCEN |
-                  RCC_AHB2ENR_GPIOHEN;  // XXX | RCC_AHB2ENR_ADCEN;
-  GPIOB->MODER &= ~(uint32_t{0x3F} << 6);
-  GPIOB->MODER |= uint32_t{0x22} << 6;
-  GPIOB->BSRR = 1 << 5;
-  GPIOB->BSRR = 1 << 4;
-  GPIOB->BSRR = 1 << 3;
-  while (1)
-    ;
-  while (1) {
-    uint32_t value = ((GPIOB->IDR >> 4) & 0x1);
-    //    GPIOB->BSRR = (value << 3) | (((~value) & 0x1) << (16 + 3));
-  }
-  */
 }
 
-// XXX where does this come from?
-void SystemInit() {}
-
 // XXX
+void SystemInit() {}
