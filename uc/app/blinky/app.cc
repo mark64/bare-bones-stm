aef386cbd83c15ab576847a7f82a349c5ca0a73d
youngpines <hy379@cornell.edu>
2021-01-25 02:29:48 -0800

    Broke out the HAL (works) but wordy

--- uc/app/blinky/app.cc
+++ uc/app/blinky/app.cc
@@ -8,22 +8,26 @@ extern "C" {
 #include "uc/hal/gpio.h"
 
 uint16_t delay_ms= 500;
-static GPIO_InitTypeDef gpio_struct;
+static uc::hal::Gpio::GpioPinStruct gpio_struct;
 
 void uc::app::Main(void) {
-    gpio_struct.Mode = GPIO_MODE_OUTPUT_PP;
-    gpio_struct.Pull = GPIO_PULLUP;
-    gpio_struct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    gpio_struct.Pin = GPIO_PIN_5;
+    GPIO_InitTypeDef pin_desc;
+    pin_desc.Mode = GPIO_MODE_OUTPUT_PP;
+    pin_desc.Pull = GPIO_PULLUP;
+    pin_desc.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    pin_desc.Pin = GPIO_PIN_5;
+
+    gpio_struct.port = GPIOA;
+    gpio_struct.pin  = &pin_desc;
  
-    uc::hal::Gpio::SetMode(GPIOA, &gpio_struct);
+    uc::hal::Gpio::SetMode(&gpio_struct);
     GPIO_PinState on = GPIO_PIN_SET;
     GPIO_PinState off = GPIO_PIN_RESET;
 
     while(1) {
-        uc::hal::Gpio::Set(GPIOA, GPIO_PIN_5, off);
+        uc::hal::Gpio::Set(&gpio_struct, off);
         HAL_Delay(delay_ms); // Time in ms
-        uc::hal::Gpio::Set(GPIOA, GPIO_PIN_5, on);
+        uc::hal::Gpio::Set(&gpio_struct, on);
         HAL_Delay(delay_ms);
     } 
 }
